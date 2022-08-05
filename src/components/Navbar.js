faad356362721e9ccc2f16e9413442868f09b045
iug-nm <guilhemmanchon@gmail.com>
2022-08-05 15:11:14 +0200

    profile pics & begining of data.json parsing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     return(
         <div id="navbar">
             <div id="site-logo">
-                <a><img alt='logo' src='https://clipartcraft.com/images/white-twitter-logo-icon-8.png' /></a>
+                <a href='http://twitter.com'><img alt='logo' src='https://clipartcraft.com/images/white-twitter-logo-icon-8.png' /></a>
             </div>
             <div id="site-menu">
                 <nav>
@@ -20,6 +20,7 @@ export default function Navbar() {
                 <a>Tweet</a>
             </div>
             <div className='navbar-account'>
+                <img alt='' src='https://cdn.pixabay.com/photo/2020/11/25/14/38/girl-5775940_960_720.jpg'/>
                 <a>Julius in the Coolius</a>
                 <h6>iug_nm</h6>
             </div>
--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function User(props) {
     return(
             <div className='post-user-infos'>
-                <img alt={props.user.name} src={props.user.img}/>
+                <img alt={props.user.name} src = {props.user.img}/>
                 <h4>{props.user.name}</h4>
                 <h6>{props.user.account_name} - {props.user.time}m</h6>
             </div>
