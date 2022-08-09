97920eb2ccfdb99b414a7eeb6a6eb05da32b6958
iug-nm <guilhemmanchon@gmail.com>
2022-08-09 10:05:42 +0200

    the data is now parsed from the data.js file, data.json may remain useless.

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import Article from './Article';
 
-let data = require('../data/data.json');
-
-// for (let key in data) {
-//     if (data.hasOwnProperty(key)) {
-//         console.log(data[key].user.name);
-//     }
-// }
+import { data } from "../data/data.js";
 // TODO Si c'est un lien faire le style correspondant 
 // TODO Si c'est une mention faire le style correspondant aussi 
 // TODO Est-on obligé de mettre à chaque fois un const par valeurs que l'on veut passer 
 
 export default function Thread() {
-    const user = {
-        name: 'h',
-        img: 'https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:647022?quality=0.8&format=jpg&width=1440&height=810&.jpg',
-        account_name: 'halsey',
-        time: 2,
-    }
-    const content = 'I mean that\'s a very good opinion right here from @iug_nm';
-    const controls = {
-        comment: "905",
-        retweet: "5.2k",
-        star: "17.8k",
-    }
+    // const user = {
+    //     name: 'h',
+    //     img: 'https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:647022?quality=0.8&format=jpg&width=1440&height=810&.jpg',
+    //     account_name: 'halsey',
+    //     time: 2,
+    // }
+    // const content = 'I mean that\'s a very good opinion right here from @iug_nm';
+    // const controls = {
+    //     comment: "905",
+    //     retweet: "5.2k",
+    //     star: "17.8k",
+    // }
 
     /**
      * Si nous récupérons le fichier data.json sans soucis et que celui-ci n'est pas vide,
@@ -32,86 +26,27 @@ export default function Thread() {
      *créer un tableau contenant tous les articles que nous aurons récupérer de data.json
      */
 
-     let article_list = [
-        <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-        />,
-        <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-        />,
-        <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-        />,
-        <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-        />,
-        <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-        />,
-     ];
-
     return(
         <>
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-            <Article 
-                user = {user} 
-                content = {content}
-                controls = {controls}
-            />
-    </>
-    )
-}
+        {/* https://www.pluralsight.com/guides/load-and-render-json-data-into-react-components */}
+        {data.map((data, key) => {
+            return(
+                <div key={key}>
+                    {data.content + ", " + data.user.name}
+                </div>
+            );
+        })}
+        </>
+    //     <>
+    //         <Article 
+    //             user = {user} 
+    //             content = {content}
+    //             controls = {controls}
+    //         />
+    //         <Article 
+    //             user = {user} 
+    //             content = {content}
+    //             controls = {controls}
+    //         />
+    );
+};
---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        user: {
+            name: "h",
+            img: "https://mtv.mtvnimages.com/uri/mgid:ao:image:mtv.com:647022?quality=0.8&format=jpg&width=1440&height=810&.jpg",
+            account_name: "halsey",
+            time: 2
+        },
+        content: "I mean that's a very good opinion right here from @iug_nm",
+        controls: {
+            comment: 905,
+            retweet: "5.2k",
+            star: "17.8k",
+        }
+    },
+    {
+        user: {
+            name: "Joy Crookes",
+            img: "https://s2.qwant.com/thumbr/0x380/4/9/a9814dea22693bc24cb393ba2023ec48fab2d89f3b390c7f7892c12d283fde/joy-crookes-e1568802067346.jpg?u=https%3A%2F%2Fi1.wp.com%2Fwww.goldenplec.com%2Fwp-content%2Fuploads%2F2019%2F09%2Fjoy-crookes-e1568802067346.jpg%3Ffit%3D1400%252C933%26ssl%3D1&q=0&b=1&p=0&a=0",
+            account_name: "joy_crookes",
+            time: 5
+        },
+        content: "Wish you were mine you here now https://www.youtube.com/watch/a9fea894e",
+        controls: {
+            comment: 152,
+            retweet: "456",
+            star: "5.4k",
+        }
+    },
+]
