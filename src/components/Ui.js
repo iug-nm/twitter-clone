982e3813316797d374ff85824c03da83d2871635
iug-nm <guilhemmanchon@gmail.com>
2022-09-08 13:31:40 +0200

    test files added

---
+++ src/components/Ui.js
@@ -0,0 +1,11 @@
+// import react from 'react';
+// import '../style.css'
+
+export default function Ui() {
+    return (
+        <div id='ui'>
+        <button>click me</button>
+        <input placeholder='recherchez'></input>
+        </div>
+    )
+}
