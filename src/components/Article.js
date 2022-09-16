75e49d4620c0827a4c7c206331b553aac35f438a
iug-nm <guilhemmanchon@gmail.com>
2022-09-16 15:55:04 +0200

    fixed a problem where post content would render multiple times + created a function to clean urls

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,66 +3,6 @@ import User from './User';
 
 export default function Article(props) {
 
-    const setAttributes = (element, attributes) => {
-        for (let key in attributes) {
-            element.setAttribute(key, attributes[key]);
-        }
-    }
-
-    // const finder = (content) => {
-
-    //     let temp_content = content
-    //     var message = document.createElement('p');
-    //     const reg = [
-    //         /(?:^|[^a-zA-Z0-9_@])(@)(?!\.)([a-zA-Z0-9_\.]{1,15})(?:\b(?!@)|$)/g,
-    //         /[-a-zA-Z0-9@:%_\+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi
-    //     ];
-
-    //     reg.forEach((e) => {
-    //         let res = temp_content.match(e);
-    //         let _content = temp_content.split(res);
-    //         console.log(_content);
-    //         console.log(res);
-    //         if (res != null) {
-    //             res.forEach((elem) => {
-    //                 let node = document.createElement('a');
-    //                 node.innerHTML = elem;
-    //                 setAttributes(node, {
-    //                     'class': 'linked',
-    //                     //'href': urlCleaner(elem),
-    //                 });
-    //                 message.innerHTML = _content[0] + '';
-    //                 message.appendChild(node);
-    //                 message.innerHTML += _content[1];
-    //             });
-    //         } 
-    //     }); 
-    //     //BUG L'écran devient blanc lorsque je veux mount le component une deuxième fois ??!
-    //     //FIXME Ne retourner me message en entier que lorsque l'on a formatté toutes les occurences de ce dernier
-    //     //document.querySelector("article > a[href='./profile/" + props.user.account_name + "'] + .post-content").appendChild(message);
-    // }
-
-    const finder = (content) => {
-        const reg = [
-            /(?:^|[^a-zA-Z0-9_@])(@)(?!\.)([a-zA-Z0-9_.]{1,15})(?:\b(?!@)|$)/g,
-            /[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?/gi
-        ];
-
-        reg.forEach((e) => {
-            let res = content.match(e);
-            if (res != null) {
-                res.forEach((elem) => {
-                    content = content.split(elem);
-                    content = content[0] + " <a href='" + elem.replace('@', '') + "' class='link'>" + elem + "</a>" + content[1];
-                });
-            }
-        });
-        let p = document.createElement('p');
-        p.innerHTML = content
-        document.querySelector("article > a[href='./profile/" + props.user.account_name + "'] + .post-content").appendChild(p);
-    }
-
-
     return(
         <article className='post' id={props.id}>
            <User 
@@ -70,7 +10,7 @@ export default function Article(props) {
                 posts = {props.posts} 
             />
                 <div className='post-content'>
-                    {finder(props.content)}
+                    {props.content}
                 </div>
             <Controls controls = {props.controls} />
         </article>
--- src/components/Thread.js
+++ src/components/Thread.js
@@ -7,6 +7,30 @@ import { posts } from '../data/posts'
 
 export default function Thread() {
 
+    const clean = (content) => {
+        return content.replace(/^(http|https):\/\/www./, '');
+    }
+
+    const content_format = (content) => {
+        const reg = [
+            /(?:^|[^a-zA-Z0-9_@])(@)(?!\.)([a-zA-Z0-9_.]{1,15})(?:\b(?!@)|$)/g,
+            /[-a-zA-Z0-9@:%_+.~#?&/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_+.~#?&//=]*)?/gi
+        ];
+
+        reg.forEach((e) => {
+            let res = content.match(e);
+            if (res != null) {
+                res.forEach((elem) => {
+                    content = content.split(elem);
+                    content = content[0] + " <a href='" + elem.replace('@', '') + "' class='link'>" + clean(elem) + "</a>" + content[1];
+                });
+            }
+        });
+        // let p = document.createElement('p');
+        // p.innerHTML = content
+        return content;
+    }
+
     return(
         <>
         {/* https://stackoverflow.com/questions/41311322/how-can-i-map-over-two-arrays-at-the-same-time */}
@@ -19,7 +43,7 @@ export default function Thread() {
                     id = {content.post.post_time+'-'+index}
                     user = {users.user}
                     posts = {content.post.post_time}
-                    content = {content.post.post_content}
+                    content = {content_format(content.post.post_content)}
                     controls = {content.post.reactions}
                 />
             )
